23b0fd5a7f677fbe3caee5f60cc9486462db44ae
GuilhermeHorta22 <ghorta800@gmail.com>
2025-09-06 14:36:41 -0300

    pagina de detalhes das tasks

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; //biblioteca que vai auxiliar na criação de telas para meu front
+import TaskPages from './pages/TaskPages.jsx';
 
 //criando uma rota inicial
 const router = createBrowserRouter([
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     path: "/", //caminho da rota
     element: <App /> //elemento que vai ser chamado quando acessar a rota
   },
+  {
+    path: "/task",
+    element: <TaskPages />
+  }
 ]);
 
 
--- src/pages/TaskPages.jsx
+++ src/pages/TaskPages.jsx
@@ -1,7 +1,29 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 function TaskPages()
 {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const title = searchParams.get("title");
+    const description = searchParams.get("description");
     return (
-        <h1>Task pages</h1>
+    <div className="w-screen h-screen bg-slate-900 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <div className="flex text-white mb-6">
+            <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
+                <ChevronLeftIcon />
+            </button>
+        </div>
+
+        <h1 className="text-3xl text-slate-100 font-bold text-center" >Detalhes da tarefa</h1>
+
+        <div className="bg-slate-400 p-4 rounded-md">
+            <h2 className="text-xl text-slate-700 font-bold">{title}</h2>
+            <p className="text-slate-700">{description}</p>
+        </div>
+      </div>
+    </div>
     );
 }
 
